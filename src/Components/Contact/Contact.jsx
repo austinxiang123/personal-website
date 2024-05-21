b2db798d1f4814488c12f684c7dbe5b07e6c10a9
austinxiang123 <89711315+austinxiang123@users.noreply.github.com>
2024-05-21 16:30:25 -0500

    fix: submission functionality

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", REACT_APP_WEB3_ACCESS_KEY);
+    formData.append("access_key", process.env.REACT_APP_WEB3_ACCESS_KEY);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
